990e75cd0fe1cdbdfaed140f51ecf27303b59ae6
John <john.njoroge@student.moringaschool.com>
2022-04-05 15:30:28 +0300

    ft: making the delete button functional

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -11,9 +11,16 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export class TaskItemComponent implements OnInit {
   @Input() task!: Task;
   @Input() color = 'red';
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter
 
   faTimes = faTimes;
 
+  onDelete(task) {
+    confirm("Do you want to remove this task?")
+    // let de = this.task
+    this.onDeleteTask.emit(task);
+    
+  }
 
   constructor() { }
 
